937df51f9377c90c32fa6aa2651cefef0da83710
SPBpiraT <intake@list.ru>
2025-08-07 04:13:40 +0300

    Added few models, views and ResourceController for testing view markups.

---
+++ Warehouse.Web/Controllers/ResourceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Warehouse.Web.Models.Resource;
+
+namespace Warehouse.Web.Controllers
+{
+    public class ResourceController : Controller
+    {
+        private readonly ILogger<ResourceController> _logger;
+
+        public ResourceController(ILogger<ResourceController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Resource()
+        {
+
+            return View(new ResourceViewModel { Title = "Banana"});
+        }
+
+        public IActionResult Resources()
+        {
+            return View(new ResourcesListViewModel
+            {
+                ResourcesList = new List<ResourceViewModel>()
+                {
+                    new ResourceViewModel { Title = "Peanut" },
+                    new ResourceViewModel { Title = "Cherry" }
+                }
+            });
+        }
+    }
+}
