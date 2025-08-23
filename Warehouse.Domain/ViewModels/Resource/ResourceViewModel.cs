b4c8c0dd154b8db077c4738f02544900e121d68e
SPBpiraT <intake@list.ru>
2025-08-23 09:50:33 +0300

    Added Display attribute for ResourceViewModel

--- Warehouse.Domain/ViewModels/Resource/ResourceViewModel.cs
+++ Warehouse.Domain/ViewModels/Resource/ResourceViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace Warehouse.Domain.ViewModels.Resource
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Warehouse.Domain.ViewModels.Resource
 {
     public record ResourceViewModel
     {
         public int Id { get; set; }
+
+        [Display(Name = "Наименование")]
         public string Title { get; set; }
+
         public bool IsActive { get; set; }
     }
 }
