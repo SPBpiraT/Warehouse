04ecfd82924a8c35c41b42a1dccfce7c82efb908
SPBpiraT <intake@list.ru>
2025-08-23 20:50:50 +0300

    Added Unit controller, Unit ViewModels and Views

---
+++ Warehouse.Domain/ViewModels/Unit/UnitViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Warehouse.Domain.ViewModels.Unit
+{
+    public record UnitViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Наименование")]
+        public string Title { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
