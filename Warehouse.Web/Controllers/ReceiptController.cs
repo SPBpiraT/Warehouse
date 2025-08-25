a7babbaf37cf1ce48b3abbec179df34b41748d1d
SPBpiraT <intake@list.ru>
2025-08-25 21:11:31 +0300

    Added Balance, Shipment, Receipt controllers

---
+++ Warehouse.Web/Controllers/ReceiptController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Warehouse.Web.Controllers
+{
+    public class ReceiptController : Controller
+    {
+        private readonly ILogger<ReceiptController> _logger;
+        private readonly IMemoryCache _memoryCache;
+
+        public ReceiptController(ILogger<ReceiptController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+    }
+}
---
+++ Warehouse.Web/Controllers/ShipmentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Warehouse.Web.Controllers
+{
+    public class ShipmentController : Controller
+    {
+        private readonly ILogger<ShipmentController> _logger;
+        private readonly IMemoryCache _memoryCache;
+
+        public ShipmentController(ILogger<ShipmentController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+    }
+}
