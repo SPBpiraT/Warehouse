a9167248205d6c2d761ddac8acc9840ec563f822
SPBpiraT <intake@list.ru>
2025-09-06 15:25:03 +0300

    Added Receipt entities

---
+++ Warehouse.Domain/Entities/ReceiptItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Warehouse.Domain.Entities
+{
+    public record ReceiptItem
+    {
+        public int Id { get; set; }
+        public int ReceiptId { get; set; }
+        public int ResourceId { get; set; }
+        public int UnitId { get; set; }
+        public Resource? Resource { get; set; }
+        public Unit? Unit { get; set; }
+        public ulong Quantity { get; set; }
+    }
+}
