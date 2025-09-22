a1ea2acbc9e3e6426a29a05985765fb7029f2318
SPBpiraT <intake@list.ru>
2025-09-22 23:52:05 +0300

    Added receipt edit view

---
+++ Warehouse.Domain/ViewModels/Receipt/EditReceiptViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Warehouse.Domain.ViewModels.Receipt
+{
+    public record EditReceiptViewModel
+    {
+        public Entities.Receipt Receipt { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> Resources { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> Units { get; set; }
+    }
+}
